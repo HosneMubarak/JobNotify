7844558de3c077b7fce29f444be12e0823715cd6
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2025-04-24 18:25:28 +0600

    add load dot env

--- JobNotify/settings.py
+++ JobNotify/settings.py
@@ -15,6 +15,10 @@ from django.utils import timezone
 from pathlib import Path
 from dotenv import load_dotenv
 
+# Load environment variables from the .env file
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
